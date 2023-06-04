31f2157bd1e334e7196fcaf0a70dc3a3cfd79cab
cristiano.dias.spbr1982 <cristiano@qualyteam.com.br>
2023-06-04 13:10:52 -0300

    Create new SharedKernel project with Result class

---
+++ src/libs/SharedKernel/Result.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedKernel
+{
+    public record Result
+    {
+        public bool Succeeded { get; set; }
+        public string? Message { get; set; }
+        public Exception? Exception { get; set; }
+    }
+    public record Result<T> : Result
+    {
+        public T? Value { get; init; }
+    }
+}
